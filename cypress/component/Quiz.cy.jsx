7ef158c7ff7f8a1b3c471207824d99cff6b4d4fd
bungin <nimai.band@gmail.com>
2024-11-15 00:52:01 -0600

    init2, delted wrong repo

---
+++ cypress/component/Quiz.cy.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Quiz from "./client/src/components/Quiz.tsx";
+import { describe } from "vitest";
+
+describe("Quiz", ({ test }) => {
+    it('should start with a single button', () => {
+        
+    })
+}
