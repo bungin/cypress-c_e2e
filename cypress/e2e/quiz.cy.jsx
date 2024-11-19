98cb14a833b7f0758eb29d4b944bd7da9aeba55b
bungin <nimai.band@gmail.com>
2024-11-19 02:35:13 -0600

    e2e cleanup, dnr

--- cypress/e2e/quiz.cy.jsx
+++ cypress/e2e/quiz.cy.jsx
@@ -1,64 +1,32 @@
-import React from "react";
-// import { describe } from "vitest";
 
-// probably cy.wrap the initial button
-
-describe("Quiz", () => {
-    it('should start with a single button', () => {
+describe("Quiz e2e test", () => {
+    it('Will run through the quiz, testing each button in sequence.', () => {
         cy.visit('/')
         cy.get('button').should('contain.text', 'Start Quiz').click()
-        cy.get('[data-test="test-item-0"] button').click()
-        cy.get('[data-test="test-item-0"] button').click()
-        cy.get('[data-test="test-item-0"] button').click()
-        cy.get('[data-test="test-item-0"] button').click()
-        cy.get('[data-test="test-item-0"] button').click()
-        cy.get('[data-test="test-item-0"] button').click()
-        cy.get('[data-test="test-item-0"] button').click()
-        cy.get('[data-test="test-item-0"] button').click()
-        cy.get('[data-test="test-item-0"] button').click()
-        cy.get('[data-test="test-item-0"] button').click()
+        for (let i = 0; i < 10; i++) {
+            cy.get('[data-test="test-item-0"] button').click()
+        }
         cy.get('h2').should('contain.text', 'Quiz Completed')
         cy.get('div[class="alert alert-success"]').invoke('text').should('match', /Your Score: \d+\/10/i)
         
         cy.get('button').should('contain.text', 'Take New Quiz').click()
-        cy.get('[data-test="test-item-1"] button').click()
-        cy.get('[data-test="test-item-1"] button').click()
-        cy.get('[data-test="test-item-1"] button').click()
-        cy.get('[data-test="test-item-1"] button').click()
-        cy.get('[data-test="test-item-1"] button').click()
-        cy.get('[data-test="test-item-1"] button').click()
-        cy.get('[data-test="test-item-1"] button').click()
-        cy.get('[data-test="test-item-1"] button').click()
-        cy.get('[data-test="test-item-1"] button').click()
-        cy.get('[data-test="test-item-1"] button').click()
+        for (let i = 0; i < 10; i++) {
+            cy.get('[data-test="test-item-1"] button').click()
+        }
         cy.get('h2').should('contain.text', 'Quiz Completed')
         cy.get('div[class="alert alert-success"]').invoke('text').should('match', /Your Score: \d+\/10/i)
         
         cy.get('button').should('contain.text', 'Take New Quiz').click()
-        cy.get('[data-test="test-item-2"] button').click()
-        cy.get('[data-test="test-item-2"] button').click()
-        cy.get('[data-test="test-item-2"] button').click()
-        cy.get('[data-test="test-item-2"] button').click()
-        cy.get('[data-test="test-item-2"] button').click()
-        cy.get('[data-test="test-item-2"] button').click()
-        cy.get('[data-test="test-item-2"] button').click()
-        cy.get('[data-test="test-item-2"] button').click()
-        cy.get('[data-test="test-item-2"] button').click()
-        cy.get('[data-test="test-item-2"] button').click()
+        for (let i = 0; i < 10; i++) {
+            cy.get('[data-test="test-item-2"] button').click()
+        }
         cy.get('h2').should('contain.text', 'Quiz Completed')
         cy.get('div[class="alert alert-success"]').invoke('text').should('match', /Your Score: \d+\/10/i)
         cy.get('button').should('contain.text', 'Take New Quiz').click()
 
-        cy.get('[data-test="test-item-3"] button').click()
-        cy.get('[data-test="test-item-3"] button').click()
-        cy.get('[data-test="test-item-3"] button').click()
-        cy.get('[data-test="test-item-3"] button').click()
-        cy.get('[data-test="test-item-3"] button').click()
-        cy.get('[data-test="test-item-3"] button').click()
-        cy.get('[data-test="test-item-3"] button').click()
-        cy.get('[data-test="test-item-3"] button').click()
-        cy.get('[data-test="test-item-3"] button').click()
-        cy.get('[data-test="test-item-3"] button').click()
+        for (let i = 0; i < 10; i++) {
+            cy.get('[data-test="test-item-3"] button').click()
+        }
         cy.get('h2').should('contain.text', 'Quiz Completed')
         cy.get('div[class="alert alert-success"]').invoke('text').should('match', /Your Score: \d+\/10/i)
         cy.get('button').should('contain.text', 'Take New Quiz')
